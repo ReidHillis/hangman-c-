00b856955e9dab2ecdbc2d8a0b04f8b832485fbe
ReidHillis <reidhillis7@gmail.com>
2025-09-15 13:32:17 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,12 @@ using namespace std;
 
 int main() {
     cout << "Welcome to Hangman!" << endl;
-    cout << "Guess letters to reveal the secret word." << endl;
+    cout << "Guess one letter at a time to reveal the secret word." << endl;
     cout << "Solve the word before the hangman is drawn.\n" << endl;
 
     Game game;
     game.startGame(); // Start the game
 
     cout << "\nThanks for playing!" << endl;
+
 }
